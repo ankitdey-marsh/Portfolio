b22608df65aa412bdb97b28beb8bcb3e0a7e7607
Ankit Dey <personal.ankitdey@gmail.com>
2024-10-19 20:54:32 +0530

    Update resume.jsx

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -4,7 +4,7 @@ import { IoNewspaperOutline } from "react-icons/io5";
 const resume = () => {
   return (
       <div className='border w-48 h-12 border-[#00000095] hover:border-[#9956C4] relative left-0 md:left-6 rounded-full visible group hover:text-white ease-in-out overflow-hidden md:top-0 top-6'>
-        <a className=''  href="https://drive.google.com/file/d/1ZLIbqg1Wlxx0Wtdile7J5fTDv6pYHBHi/view?usp=drive_link">
+        <a className=''  href="https://drive.google.com/file/d/1pjU6kEZkBtgYhXjKEGpPOCqQX5vtgHo9/view?usp=sharing">
             <div className='md:w-42 w-40 bg-gradient-to-t from-[#6002b8bb] to-[#a858b3] h-24 absolute md:left-16 left-20 rotate-90 origin-bottom-right group-hover:-rotate-0 transition-all duration-500 ease-out group-hover:rounded-full group-hover:scale-150 md:group-hover:translate-x-16 group-hover:translate-x-12 group-hover:w-64 group-hover:translate-y-0'>
                 
             </div>
